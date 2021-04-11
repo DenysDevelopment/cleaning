062388faab2bfd81011c8d0eeb0f3beb36dd58da
DenysDevelopment <dinis_05@ukr.net>
2021-04-11 13:34:51 +0300

    created fine

--- js/index.js
+++ js/index.js
@@ -2,9 +2,17 @@ const URL = 'https://cherguvannya-f8c7b-default-rtdb.firebaseio.com/db.json';
 const out = document.querySelector('.users');
 let data;
 
+// function sortByCount(arr) {
+// 	return arr.sort((a, b,) => a.count > b.count ? 1 : -1);
+// }
 function sortByCount(arr) {
-	return arr.sort((a, b) => a.count > b.count ? 1 : -1);
+	return arr.sort((a, b,) => a.count > b.count ? 1 : -1);
 }
+
+function sortByFine(arr) {
+	return arr.sort((a, b) => a.countFine > 0 ? 1 : -1);
+}
+
 
 async function getData(url) {
 	const dataResponse = await fetch(url);
@@ -21,8 +29,15 @@ getData(URL)
 function renderUsers() {
 
 
-	sortByCount(Object.values(data)).forEach(el => {
+	sortByCount(sortByFine(Object.values(data))).forEach(el => {
 		out.innerHTML += `<li class='users__item' data-date='${el.lastDate}'>
+			<div class="users__fine" style='color:
+			${
+				el.countFine === 0 ? '#fff' :
+				 el.countFine == 1 ? '#ff3838' :
+				 el.countFine == 2 ? '#ff1717' : '#ff0000'
+			}'
+			>Штрафи: ${el.countFine}</div>
 			<div class="users__name">${el.name}</div>
 			<div class="users__count">Чергував: ${el.count}</div>
 			<div class="users__wash">Мив: ${el.countWash}</div>
