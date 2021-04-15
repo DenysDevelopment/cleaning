7b43d3e6b4dd5b7598e02dc71c2d677e88a9a732
DenysDevelopment <dinis_05@ukr.net>
2021-04-15 09:25:47 +0300

    gradient animation fix and done to production

--- js/index.js
+++ js/index.js
@@ -1,53 +1,53 @@
-const URL = "https://cherguvannya-f8c7b-default-rtdb.firebaseio.com/db.json"
-const out = document.querySelector(".users")
-let data
+const URL = "https://cherguvannya-f8c7b-default-rtdb.firebaseio.com/db.json";
+const out = document.querySelector(".users");
+let data;
 
 // function sortByCount(arr) {
 // 	return arr.sort((a, b,) => a.count > b.count ? 1 : -1);
 // }
 function sortByCount(arr) {
-  return arr.sort((a, b) => (a.count > b.count ? 1 : -1))
+  return arr.sort((a, b) => (a.count > b.count ? 1 : -1));
 }
 
 function sortByFine(arr) {
-  return arr.sort((a, b) => (a.countFine < 0 ? 1 : -1))
+  return arr.sort((a, b) => (a.countFine < 0 ? 1 : -1));
 }
 
 async function getData(url) {
-  const dataResponse = await fetch(url)
-  return dataResponse
+  const dataResponse = await fetch(url);
+  return dataResponse;
 }
 
 getData(URL)
   .then((data) => data.json())
   .then((response) => {
-    data = response
-    renderUsers()
-  })
+    data = response;
+    renderUsers();
+  });
 
 function renderUsers() {
-  out.innerHTML = ""
+  out.innerHTML = "";
   sortByCount(Object.values(data)).forEach((el) => {
     out.innerHTML += `<li class='users__item' data-date='${el.lastDate}'>
 			<div class="users__name">${el.name}</div>
 			<div class="users__count">Чергував: ${el.count}</div>
 			<div class="users__wash">Мив: ${el.countWash}</div>
 			<div class="users__sweep">Замітав: ${el.countSweep}</div>
-		</li>`
-  })
+		</li>`;
+  });
 }
 
-const btnUsertabElem = document.querySelector(".all-user")
+const btnUsertabElem = document.querySelector(".all-user");
 
-btnUsertabElem.addEventListener("click", renderUsers)
+btnUsertabElem.addEventListener("click", renderUsers);
 
 //========================================================================================================================================================
 //fine
 
-const btnFinetabElem = document.querySelector(".fine")
+const btnFinetabElem = document.querySelector(".fine");
 
 btnFinetabElem.addEventListener("click", () => {
-  out.innerHTML = ""
+  out.innerHTML = "";
   sortByFine(sortByCount(Object.values(data))).forEach((el) => {
     if (el.countFine > 0) {
       out.innerHTML += `<li class='users__item' data-date='${el.lastDate}'>
@@ -66,26 +66,10 @@ btnFinetabElem.addEventListener("click", () => {
 			<div class="users__count">Чергував: ${el.count}</div>
 			<div class="users__wash">Мив: ${el.countWash}</div>
 			<div class="users__sweep">Замітав: ${el.countSweep}</div>
-		</li>`
+		</li>`;
     }
-  })
+  });
   if (!out.innerHTML.length) {
-    out.innerHTML = "<div class='not-found'>Немає штрафів</div>"
+    out.innerHTML = "<div class='not-found'>Немає штрафів</div>";
   }
-})
-
-//change background
-// const API_KEY = '4ikKa13E4rnv2uUjr8RfUwA4J3Myoi7jMOGaucUyN4w';
-// const URL_IMG = `https://api.unsplash.com/photos/random?client_id=${API_KEY}`;
-
-// function changeBackground(url) {
-//   axios.get(url) 
-//   .then(response => {
-//     console.log(response.data.urls.raw)
-//     document.body.style.backgroundImage = `url(${response.data.urls.full})`;
-//   })
-// }
-// changeBackground(URL_IMG);
-
-//bg pattern 
-document.body.style.backgroundImage = `url(./img/${Math.floor(Math.random() * 14)}.png)`;
+});
